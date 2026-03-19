838c2578a8299b3ba1b0b4078c22e2f630e9b6c0
Neal Homan <homanne@oregonstate.edu>
2026-03-19 12:40:16 -0700

    add showsubsview file

---
+++ ScoreKeep/Games/ShowSubsView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowSubsView.swift
+//  ScoreKeep
+//
+//  Created by Neal Homan on 3/19/26.
+//
+
+import SwiftUI
+
+struct ShowSubsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowSubsView()
+}
